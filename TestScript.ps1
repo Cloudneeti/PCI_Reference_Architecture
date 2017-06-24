23026954b4a66f007b528c97ce0ff24c09c8fa99
consultguru <gururaj.pandurangi@gmail.com>
2017-06-24 00:55:04 -0700

    Fixing minor spelling errors

---
+++ TestScript.ps1
@@ -0,0 +1,15 @@
+﻿cd C:\Users\Pandurangi\Documents\GitHub\PCI_Reference_Architecture
+
+$subscriptionID = 'b4605119-4803-4924-a221-091570e36d01'
+$resourceGroupPrefix = 'rg-pci-iaas'
+$location = 'South Central US'
+$steps1_2 = @("1", "2", "3")
+$stepsOthers = @("4","5","6","7")
+
+. .\scripts\deployme.ps1
+
+Invoke-ArmDeployment -subId $subscriptionID -resourceGroupPrefix $resourceGroupPrefix -location $location -deploymentPrefix dev -steps $steps1_2
+
+Invoke-ArmDeployment -subId $subscriptionID -resourceGroupPrefix $resourceGroupPrefix -location $location -deploymentPrefix dev -steps $stepsOthers
+
+#Remove-ArmDeployment -subId $subscriptionID -rg $resourceGroupPrefix -dp dev
