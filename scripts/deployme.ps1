f62836b855c0a246473675e8a2e0e35472413d87
Zaitsev Yury <zaja1kun@gmail.com>
2017-06-25 01:16:52 +0300

    Restored previously deleted logics for using arm templates from azure storage

--- scripts/deployme.ps1
+++ scripts/deployme.ps1
@@ -201,3 +201,59 @@ $request = '{
 $scriptRoot = Split-Path ( Split-Path $MyInvocation.MyCommand.Path )
 $ProfilePath = "$scriptRoot\auth.json"
 $components = @("application", "dmz", "security", "management", "operations", "networking")
+
+
+# $AAAcct = New-AzureRmAutomationAccount -ResourceGroupName "$locationcoerced-automation" -Location $location -Name $StorageAcct -ErrorAction Stop
+
+# # Get needed Powershell DSC modules and start upload to Azure Automation directly
+# # from powershellgallery.com, need to get this list dynamically from DSC configuration
+# $modules = Find-Module -Name NX, xPSDesiredStateConfiguration, xNetworking, xWebAdministration #, PSDscResources
+# $modules | ForEach-Object {
+#     $url = 'https://www.powershellgallery.com/api/v2/package/{0}/{1}' -f $_.Name, $_.Version
+#     do {
+#         $ActualUrl = $url
+#         $Url = (Invoke-WebRequest -Uri $url -MaximumRedirection 0 -ErrorAction Ignore).Headers.Location
+#     } while ( $Url -ne $Null ) # finding actual module payload url
+
+#     $null = New-AzureRmAutomationModule -ResourceGroupName  "$locationcoerced-automation" -AutomationAccountName $StorageAcct `
+#         -Name $_.Name -ContentLink $ActualUrl -ErrorAction Stop
+# }
+
+# Get List of Files | ForEach-Object {
+#     Write-Host "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')  Importing configuration `"$_`"" -ForegroundColor Green
+#     $null = Import-AzureRmAutomationDscConfiguration -SourcePath "$scriptRoot\artifacts\$_" -Published -Force `
+#         -ResourceGroupName "$locationcoerced-automation" -AutomationAccountName $StorageAcct -ErrorAction Stop
+# }
+
+# $StorageAccount = New-AzureRmStorageAccount -ResourceGroupName "$locationcoerced-automation" -Name $StorageAcct -Type Standard_LRS `
+#     -Location $location -ErrorAction Stop # probably need to use one of the existing resource groups
+# $keys = Get-AzureRmAutomationRegistrationInfo -ResourceGroupName "$locationcoerced-automation" `
+#     -AutomationAccountName $StorageAcct -ErrorAction Stop
+# $data = Get-DeploymentData
+# $StorageAccount | New-AzureStorageContainer -Name payload -Permission Container | Out-Null
+# Get-ChildItem $scriptRoot\nestedTemplates -Filter *.json | ForEach-Object {
+#     $null = Set-AzureStorageBlobContent -Context $StorageAccount.Context -Container payload -File $_.FullName -ErrorAction Stop
+# }
+
+# $modules | ForEach-Object {
+#     do {
+#         Write-Host "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')  Waiting for module import to succeed" -ForegroundColor DarkYellow; Start-Sleep 10
+#         $uploadStatus = Get-AzureRmAutomationModule -ResourceGroupName "$locationcoerced-automation" -AutomationAccountName $StorageAcct `
+#             -Name $_.Name -ErrorAction Stop
+#     } while ( $uploadStatus.ProvisioningState -notin 'Succeeded', 'Failed')
+
+#     if ( $uploadStatus.ProvisioningState -eq 'Failed' ) {
+#         Write-Error "Module upload failed."
+#         exit 1
+#     }
+# }
+
+# # check status before creating payload vms
+# $null = Start-AzureRmAutomationDscCompilationJob -ResourceGroupName "$locationcoerced-automation" -ConfigurationName 'Ubuntu' `
+#     -AutomationAccountName $StorageAcct -ErrorAction Stop
+# $null = Start-AzureRmAutomationDscCompilationJob -ResourceGroupName "$locationcoerced-automation" -ConfigurationName 'Windows' `
+#     -AutomationAccountName $StorageAcct -ConfigurationData @{ AllNodes = @( @{ NodeName = "ssh"; Role = "BitVise" } ) } -ErrorAction Stop
+# $null = Start-AzureRmAutomationDscCompilationJob -ResourceGroupName "$locationcoerced-automation" -ConfigurationName 'Windows' `
+#     -AutomationAccountName $StorageAcct -ConfigurationData @{ AllNodes = @( @{ NodeName = "bastion"; Role = "Bastion" } ) } -ErrorAction Stop
+# $null = Start-AzureRmAutomationDscCompilationJob -ResourceGroupName "$locationcoerced-automation" -ConfigurationName 'Windows' `
+#     -AutomationAccountName $StorageAcct -ConfigurationData @{ AllNodes = @( @{ NodeName = "web"; Role = "IIS", "BitVise" } ) } -ErrorAction Stop
